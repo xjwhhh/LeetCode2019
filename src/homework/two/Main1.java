234f83b91c34b66213cb670356f256ef7df94537
xujiawei <xjwhhh233@outlook.com>
2019-11-05 17:51:23 +0800

    算法第二次课后练习

--- src/homework/two/Main1.java
+++ src/homework/two/Main1.java
@@ -3,6 +3,7 @@ package homework.two;
 import java.util.*;
 import java.util.stream.Collectors;
 
+// wrong
 public class Main1 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
@@ -18,13 +19,7 @@ public class Main1 {
         int length1 = text1.length();
         int length2 = text2.length();
         List[][] dp1=new ArrayList[length1+1][length2+1];
-//        String[][] dp1 = new String[length1 + 1][length2 + 1];
-//        for (int i = 0; i < dp1.length; i++) {
-//            for (int j = 0; j < dp1[0].length; j++) {
-//                dp1[i][j] = "";
-//
-//            }
-//        }
+
         for(int i=0;i<dp1.length;i++){
             for(int j=0;j<dp1[0].length;j++){
                 ArrayList<String> temp=new ArrayList<>();
@@ -37,22 +32,12 @@ public class Main1 {
             for (int j = 1; j < dp[0].length; j++) {
                 if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
                     dp[i][j] = dp[i - 1][j - 1] + 1;
-//                    System.out.println(dp1[i - 1][j - 1]);
-//                    StringBuilder temp = new StringBuilder((String) dp1[i - 1][j - 1]);
-//                    temp.append(text1.charAt(i - 1));
-//                    dp1[i][j] = temp.toString();
+
                     ArrayList<String> temp1=new ArrayList<>();
                     ArrayList<String> temp2=new ArrayList<>(dp1[i-1][j-1]);
-                    ArrayList<String> result=new ArrayList<>();
-//                    for(int k=0;k<temp1.size();k++){
-//                        for(int p=0;p<temp2.size();p++){
-//                            result.add(temp1.get(k)+temp2.get(p));
-//                        }
-//                    }
                     for(String s:temp2){
                         temp1.add(s + text1.charAt(i - 1));
                     }
-
                     dp1[i][j]=temp1;
                 } else {
                     if (dp[i][j - 1] > dp[i - 1][j]) {
@@ -76,30 +61,11 @@ public class Main1 {
                                 result.add(temp2.get(k));
                             }
                         }
+                        dp1[i][j]=result;
                     }
                 }
             }
         }
-////        return dp[length1][length2];
-//        int max = dp[length1][length2];
-//        if (max == 0) {
-//            return;
-//        }
-//        ArrayList<String> result = new ArrayList<>();
-//        for (int i = 0; i < dp.length; i++) {
-//            for (int j = 0; j < dp[0].length; j++) {
-////                System.out.println(dp1[i][j]);
-//                if (dp[i][j] == max && !result.contains(dp1[i][j])) {
-//                    result.add(dp1[i][j]);
-////                    for(int m=0;m<dp1[i][j].size();m++) {
-////                        String temp=(String)dp1[i][j].get(m);
-////                        if(!result.contains(temp)) {
-////                            result.add(temp);
-////                        }
-////                    }
-//                }
-//            }
-//        }
 
         List<String> results =((ArrayList<String>) dp1[length1][length2]).stream()
                 .filter(s -> !s.isEmpty())
@@ -108,9 +74,5 @@ public class Main1 {
         for (String res : results) {
             System.out.println(res);
         }
-
-
-
-
     }
 }
